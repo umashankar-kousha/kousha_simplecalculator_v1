2b7f8d5b6b4a50c17b387001b2b9af4abd900a14
uma shankar <umashankar.kousha@gmail.com>
2025-09-22 14:37:12 +0530

    Improved functionality ,some input error handling & styling

--- app.js
+++ app.js
@@ -10,7 +10,50 @@ function onButtonClick(val) {
   displayEl.value += val;
 }
 
+function onSquareButtonClick() {
+  let value = displayEl.value;
+  if (value == "") {
+    alert("Error : Please enter number first");
+  } else {
+    displayResultEl.value = value ** 2;
+  }
+}
+
+function onCubeButtonClick() {
+  let value = displayEl.value;
+  if (value == "") {
+    alert("Error : Please enter number first");
+  } else {
+    displayResultEl.value = value ** 3;
+  }
+}
+
+displayEl.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+
+    onEquallButtonClick();
+  }
+});
+
 function onEquallButtonClick() {
-  let result = eval(displayEl.value);
-  displayResultEl.value = result;
+  let end = inputValidation(displayEl.value.at(-1));
+  let start = inputValidation(displayEl.value[0]);
+  if (start || end) {
+    alert("Error:Please give valid format");
+  } else {
+    let result = eval(displayEl.value);
+    displayResultEl.value = result;
+  }
+}
+
+function inputValidation(symbol) {
+  const mainString = symbol;
+  const targetStrings = ["+", "-", "*", "/"];
+
+  const foundAny = targetStrings.some((substring) => {
+    return mainString.includes(substring);
+  });
+
+  return foundAny;
 }
